4f5779937de651650d90f7d286f0b957db53cb38
Dje Par <jparadis21@hotmail.com>
2022-10-14 15:57:07 +0800

    Cmatrix is closer but still not working all

--- scripts/cmatrix.js
+++ scripts/cmatrix.js
@@ -1,23 +1,28 @@
 // first of all, trying to generate a random number and converting it to it's char
 
 function randomChar(){
-    num = Math.floor(Math.random() * 200) + 33;
+    num = Math.floor(Math.random() * 100) + 33;
     return String.fromCharCode(num);
 }
-
-function createChar(){
-    const rand = randomChar()
-    const newChar = document.createElement("p");
-    const container = document.getElementById("container");
+function generateOne(i){
+    let container = document.getElementById(`container${i}`);
+    let rand = randomChar()
+    let  newChar = document.createElement("p");
     newChar.innerHTML = rand
     container.appendChild(newChar)
     console.log(rand)
+}
+function generateAll(){
+
+    for (let i = 0; i < 30; i++){
+        setTimeout(generateOne(i), 380)
+    }
+    
+
    
 }
 
-setInterval(createChar, 380)
-
-
+setInterval(generateAll, 180)
 /* Creating balls or letter with a button
 let ballcontainer = document.getElementById("second-container");
 let BallCreator = document.getElementById("myBtn");
